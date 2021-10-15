24b620eb0b9a7de6928ed6c77a89ee35d2571147
candacerubin <candacerubin0@gmail.com>
2021-10-14 17:33:58 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('<h1>Hello All</h1>');
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
